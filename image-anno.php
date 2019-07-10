d6f2a2f8251194a5fa4dc6c190369c80f7082cb9
Ben Johnston <benj@princeton.edu>
2019-07-10 15:35:16 -0400

    made some updates

--- image-anno.php
+++ image-anno.php
@@ -1,298 +1,332 @@
 <?php
 /*
-Plugin Name: Princeton Image Annotator
-Plugin URI: http://www.princeton.edu
-Description:
-Version: 1.0
-Author: Ben Johnston
-Author URI: http://www.princeton.edu
-License: GPL2
+	Plugin Name: Princeton Simple Image Annotation
+	Plugin URI:
+	Description: Allow annotations to be placed on featured images by dragging a marker and adding an annotation.
+	Version: 1.0
+	Author: Ben Johnston
 */
 
 
 
-add_action('wp_enqueue_scripts','image_annotator_init');
-add_action('admin_enqueue_scripts','image_annotator_init');
 
+function pusia_add_scripts() {
+    global $post;
+    wp_enqueue_script("jquery-ui-draggable");
 
-function image_annotator_init() {
+  if(is_user_logged_in()) {
+    wp_register_script('pusia-js', plugins_url('js/pusia.js', __FILE__), array('jquery'),'1.1', true);
+    wp_enqueue_script('pusia-js');
+    wp_localize_script('pusia-js', 'scriptData', array('postid' => $post->ID, 'pluginsUrl' => plugins_url()));
 
-    wp_enqueue_script('jquery-ui-draggable');
-    wp_enqueue_script( 'image_annotator_js', plugins_url( '/js/image_annotator.js', __FILE__ ), array('jquery'));
+    wp_register_style('pusia-css', plugins_url('css/pusia.css',__FILE__ ));
+    wp_enqueue_style('pusia-css');
 
-    wp_enqueue_style( 'jquery-ui-css', "//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css" );
-    wp_enqueue_style( 'style-name', plugins_url( '/css/image_annotator.css', __FILE__ ) );
-}
-
-
-
-
-
-/***************************
-*  SHORTCODE
-***************************/
-function annotated_image_shortcode( $atts ){
-   global $post;
-
-   $image_id = get_post_meta( $post->ID, '_imganno_img', true );
-   $markers_data = get_post_meta($post->ID, "_imganno_markers", true);
-   $display_type = get_post_meta($post->ID, "_imganno_display", true);
-   if( empty($display_type) ) { $display_type = "popup"; }
-
-   if ( ! empty( $image_id ) ) {
-	$attachment_url = wp_get_attachment_url( $image_id );
-
-	$html .= "<div id='img_frame' style='position:relative;display:inline-block' style='margin-top:20px;border:solid 1px green;'>";
-	$html .= "<input type='hidden' name='_imganno_markers' id='_imganno_markers' value='{$markers_data}'/>";
-
-	foreach(json_decode($markers_data) as $key=>$val) {
-	  $id = $key;
-	  $num = str_replace("m","",$key)+1;
-	  $valArr = explode(":",$val);
-	  $top = $valArr[0];
-	  $left = $valArr[1];
-
-	  $popup_top = $valArr[0]-20;
-	  $popup_left = $valArr[1]-20;
-	  $text = get_post_meta( $post->ID, '_'.$id.'_content', true );
-
-	  $html .= "<div id='{$id}' class='imganno_marker' style='position:absolute;top:{$top}px;left:{$left}px;display:none'>{$num}</div>";
-	  if($display_type == 'popup') {
-	      $html .= "<div class='annotation_popup' id='{$id}_popup' style='display:none;position:absolute;'><div class='annotation_popup_header'><a href='#' style='text-decoration:none;'><span class='dashicons dashicons-no-alt'></span></a></div><div class='annotation_popup_content'>{$text}</div></div>";
-	  } // end if display_type
-	}
-
-	
-
-	$html .= "<img id='myimg' src='{$attachment_url}' style='width:100%' />";
-
-	if($display_type == 'footnote') {
-
-		foreach(json_decode($markers_data) as $key=>$val) {
-		  $id = $key;
-		  $num = str_replace("m","",$key)+1;
-		  $text = get_post_meta( $post->ID, '_'.$id.'_content', true );
-		  $html .= "<p  class='annotation_footnote' id='{$id}_footnote'>{$num}. {$text}</p>";
-		}
-	}
-
-	$html .= "</div>";
-
-   }
-
-  echo  $html;
-
-}
-add_shortcode( 'annotated_image', 'annotated_image_shortcode' );
-
-
-
-
-/***************************
-*  SAVE POST
-***************************/
-
-add_action( 'save_post', 'imganno_meta_box_save' );
-
-
-function imganno_meta_box_save($post_id) {
-
-    // get out if we're doing an auto save
-    if( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) return;
-
-    $post_id = $_POST['post_ID'];
-
-    if (array_key_exists('_imganno_markers', $_POST)) {
-	$marker_data = $_POST['_imganno_markers'];
-        update_post_meta(
-            $post_id,
-            '_imganno_markers',
-            $marker_data
-        );
-    }
-
-    if (array_key_exists('_imganno_img', $_POST)) {
-	$image_id = $_POST['_imganno_img'];
-        update_post_meta(
-            $post_id,
-            '_imganno_img',
-            $image_id
-        );
-    }
-
-    if (array_key_exists('_imganno_display', $_POST)) {
-	$image_id = $_POST['_imganno_display'];
-        update_post_meta(
-            $post_id,
-            '_imganno_display',
-            $image_id
-        );
-    }
-
-
-
-
-  //  update the content for the image annotation markers
-  for($x=0;$x<=9;$x++) {
-    if(isset($_POST['m'.$x.'_content'])) { 
-        update_post_meta(
-            $post_id,
-            '_m'.$x.'_content',
-            $_POST['m'.$x.'_content']
-        );
-    }
   }
-
 }
+add_action( 'wp_enqueue_scripts', 'pusia_add_scripts' );  
 
 
 
 
 
 
-/*******************************************
- * Adds a box to the main column on the Post and Page edit screens.
- *******************************************/
-function ImgAnno_add_meta_box() {
-
-	$screens = array( 'post', 'page' );
-	foreach ( $screens as $screen ) {
-		add_meta_box(
-			'ImgAnno_addlink', 'Annotated Image',
-			'ImgAnno_meta_box_content',
-			$screen
-		);
-	}
-}
-
-/*******************************************
- * Inserts new, custom featured image input
- *******************************************/
-
-function ImgAnno_book_cover_metabox ( $post ) { 
-
-?>
-
-<p>Add the following shortcode to the content area of this page to include the image and annotations: [annotated_image]</p>
+/******************************************
+* shortcode for inserting featured image
+******************************************/
 
 
-<?php
-
-
-	global $content_width, $_wp_additional_image_sizes;
-
-	$image_id = get_post_meta( $post->ID, '_imganno_img', true );
-
-	$markers_data = get_post_meta($post->ID, "_imganno_markers", true);
-
-	if ( ! empty( $image_id ) ) {
-
-		$attachment_url = wp_get_attachment_url( $image_id );
-
-		if ( ! empty( $attachment_url ) ) {
-
-			?>
-
-<div id="img_frame" style="position:relative" style="margin-top:20px;border:solid 1px green;">
-
-  <div id='m9' class='draggable'>10</div>
-  <div id='m8' class='draggable'>9</div>
-  <div id='m7' class='draggable'>8</div>
-  <div id='m6' class='draggable'>7</div>
-  <div id='m5' class='draggable'>6</div>
-  <div id='m4' class='draggable'>5</div>
-  <div id='m3' class='draggable'>4</div>
-  <div id='m2' class='draggable'>3</div>
-  <div id='m1' class='draggable'>2</div>
-  <div id='m0' class='draggable'>1</div>
-
-  <img id="myimg" src="<?php echo $attachment_url; ?>" style="width:500px" />
-
-  <p class="hide-if-no-js"><a href="javascript:;" id="remove_annotated_image_button" ><?php echo esc_html__( 'Remove annotated image', 'text-domain' ); ?></a></p>
-  <input type="hidden" id="upload_annotated_image" name="_listing_annotated_image" value="<?php echo esc_attr( $image_id ); ?>" />
-<?php
-
-		}
-
-		$content_width = $old_content_width;
-	} else {
-
-		$content = '<img id="img_frame" src="" style="width:' . esc_attr( $content_width ) . 'px;height:auto;border:0;display:none;" />';
-		$content .= '<p class="hide-if-no-js"><a title="' . esc_attr__( 'Set annotated image', 'text-domain' ) . '" href="javascript:;" id="upload_annotated_image_button" id="set-listing-image" data-uploader_title="' . esc_attr__( 'Choose an image', 'text-domain' ) . '" data-uploader_button_text="' . esc_attr__( 'Set Annotated Image', 'text-domain' ) . '">' . esc_html__( 'Set annotated image', 'text-domain' ) . '</a></p>';
-		$content .= '<input type="hidden" id="upload_annotated_image" name="annotated_image" value="" />';
-
-	}
-	$content .="</div>";
-	return $content;
-}
-
-
-
-
-
-/********************************************
- * Prints out the contents of the metabox
- *******************************************/
-function ImgAnno_meta_box_content() {
-
+/*
+function pusia_shortcode( $atts ){
   global $post;
 
-  echo ImgAnno_book_cover_metabox($post);
+  if(isset($atts['display']) && $atts['display']=='list') {  $display = 'list';  } else { $display = 'bubble'; }
 
-  $postID = $post->ID;
-  $image_id = get_post_meta( $post->ID, '_imganno_img', true );
-  $markers_data = get_post_meta($post->ID, "_imganno_markers", true);
-  $display_type = get_post_meta($post->ID, "_imganno_display", true);
-  ?>
+  if(!isset($atts['image'])) {
+	  if ( has_post_thumbnail() ) {
+	    $thumb_id = get_post_thumbnail_id();
+	    $thumb_url = wp_get_attachment_image_src($thumb_id,'thumbnail-size', true);
+	    $img = $thumb_url[0];
 
-<input type="hidden" name="_imganno_markers" id="_imganno_markers" value='<?php echo $markers_data; ?>'/>
-<input type="hidden" name="_imganno_img" id="_imganno_img" value='<?php echo $image_id; ?>'/>
 
-<input type="radio" name="_imganno_display" value="popup"<?php if($display_type=='popup'||empty( $display_type)) { echo 'checked="checked"';} ?> /> Show annotations as popups<br />
-<input type="radio" name="_imganno_display" value="footnote"<?php if($display_type=='footnote') { echo 'checked="checked"';} ?>/>  Show annotations as footnotes 
+	    $html = "<div id='pu_simple_image_annotation_frame'>";
+	    $html .= "<div id='pu_simple_image_annotation_toolbar'></div>";
+	    //$html .= "<div class='pu_simple_image_annotation_marker draggable' ><span class='dashicons dashicons-edit imganno-marker' ></span></div>";
+	    $html .= "<span class='dashicons dashicons-location imganno-marker draggable' title='drag this marker to create an annotation' style='color:lightgreen' ></span>";
+	    $html .= "<img src='".$img."' style='width:100%;'/>";
+	    $html .= pusia_toolbar();
+	    $html .= pusia_add_markers();
+	    $html .= pusia_add_bubbles();
+	    $html .= "</div>";
 
-<?php 
-  if(!empty($image_id) && !empty($markers_data) ) {
+	    return $html;
+	  }
+	  else { return "Featured image must be set."; }
+  }
+  else { // image url is indicated in atts
+	    $img = $atts['image'];
+	    $html = "<div id='pu_simple_image_annotation_frame' >";
+	    //$html .= "<div id='pu_simple_image_annotation_toolbar' style='width:45px;height:45px; background:white;position:absolute;top:5px;left:5px;'></div>";
+	    $html .= "<span class='dashicons dashicons-location imganno-marker draggable' title='drag this marker to create an annotation' style='color:lightgreen' ></span>";
 
-  // add the text areas for each marker
+	    $html .= "<img src='".$img."' style='width:100%;'/>";
+	    $html .= pusia_toolbar();
+	    $html .= pusia_add_markers();
 
-  foreach(json_decode($markers_data) as $key=>$marker) {
-    if($marker != '0:0') {
-      $num = str_replace("m","",$key)+1;
-      echo "<h4>Marker {$num}</h4>";
-      wp_editor( htmlspecialchars_decode( get_post_meta($postID, '_'.$key.'_content' , true ) ), $key.'_content', $settings = array('textarea_name'=>$key.'_content','textarea_rows'=>2,'teeny'=>true) );
+	    if($display == 'bubble') {
+	      $html .= pusia_add_bubbles();
+	    }
+	    $html .= "</div>";
+	    if($display == 'list') {
+	      $html .= pusia_add_list();
+	    }
+	    return $html;
+  }
+}
+add_shortcode( 'simple_annotated_image', 'pusia_shortcode' );
+
+*/
+
+
+
+
+
+function pusia_content_filter($content) {
+
+
+  if(is_single() && has_category('annotation')) {
+
+
+	  global $post;
+
+	  if(isset($atts['display']) && $atts['display']=='list') {  $display = 'list';  } else { $display = 'bubble'; }
+
+	  if(!isset($atts['image'])) {
+		  if ( has_post_thumbnail() ) {
+		    $thumb_id = get_post_thumbnail_id();
+		    $thumb_url = wp_get_attachment_image_src($thumb_id,'thumbnail-size', true);
+		    $img = $thumb_url[0];
+
+
+		    $html = "<div id='pu_simple_image_annotation_frame'>";
+		    $html .= "<div id='pu_simple_image_annotation_toolbar'></div>";
+		    //$html .= "<div class='pu_simple_image_annotation_marker draggable' ><span class='dashicons dashicons-edit imganno-marker' ></span></div>";
+		    $html .= "<span class='dashicons dashicons-location imganno-marker draggable' title='drag this marker to create an annotation' style='color:lightgreen' ></span>";
+		    $html .= "<img src='".$img."' style='width:100%;'/>";
+		    $html .= pusia_toolbar();
+		    $html .= pusia_add_markers();
+		    $html .= pusia_add_bubbles();
+		    $html .= "</div>";
+
+		    $content = $html.$content;
+		  }
+
+		  else { return "Featured image must be set."; }
+	  }
+	  else { // image url is indicated in atts
+		    $img = $atts['image'];
+		    $html = "<div id='pu_simple_image_annotation_frame' >";
+		    //$html .= "<div id='pu_simple_image_annotation_toolbar' style='width:45px;height:45px; background:white;position:absolute;top:5px;left:5px;'></div>";
+		    $html .= "<span class='dashicons dashicons-location imganno-marker draggable' title='drag this marker to create an annotation' style='color:lightgreen' ></span>";
+
+		    $html .= "<img src='".$img."' style='width:100%;'/>";
+		    $html .= pusia_toolbar();
+		    $html .= pusia_add_markers();
+
+		    if($display == 'bubble') {
+		      $html .= pusia_add_bubbles();
+		    }
+		    $html .= "</div>";
+		    if($display == 'list') {
+		      $html .= pusia_add_list();
+		    }
+		    $content = $html.$content;
+	  }
+
+  }
+return $content;
+}
+
+add_filter( 'the_content', 'pusia_content_filter' );
+
+
+
+
+
+
+
+
+/******************************************
+* add markers to the image
+******************************************/
+
+function pusia_add_markers() {
+  global $post;
+
+  $annotations = get_post_meta($post->ID,'img_annotation');
+  $html = "";
+  foreach($annotations as $anno) {
+    $a = json_decode($anno);
+    //$html .= "<div rel ='anno{$a->id}' class='pu_simple_image_annotation_marker gloss' style='top:{$a->top}%;left:{$a->left}%;'><span class='dashicons dashicons-location imganno-marker'></span></div>\n";
+    $html .= "<span class='dashicons dashicons-location imganno-marker' style='top:{$a->top}%;left:{$a->left}%;'rel ='anno{$a->id}'></span>\n";
+  }
+  return $html;
+}
+
+
+/******************************************
+* add bubbles to the image
+******************************************/
+
+function pusia_add_bubbles() {
+  global $post;
+  global $current_user;
+  get_currentuserinfo();
+
+  if(( is_user_logged_in() && $current_user->ID == $post->post_author ) || current_user_can('administrator')) {
+    $trashcan = true;
+  }
+
+  $html = "";
+  if($annotations = get_post_meta($post->ID,'img_annotation')) {
+
+    foreach($annotations as $anno) {
+      $a = json_decode($anno);
+      $a->annotation = str_replace("**BR**","<br />",$a->annotation);
+      $a->annotation = str_replace("**APOS**","&apos;",$a->annotation);
+      $a->annotation = str_replace("**QUOT**","&quot;",$a->annotation);
+
+      $user = get_userdata($a->userid);
+      $html .= "<div class='pu-simple-annotation-bubble' id='anno{$a->id}'>";
+      if($trashcan) {
+        $html .= "<div class='pu-simple-annotation-bubble-nav'><span style='font-size:1.3em;margin-right:0px;margin-bottom: 10px;' class='dashicons dashicons-trash pu-simple-annotation-bubble-trash' rel='anno{$a->id}' postid='{$post->ID}'></span></div>";
+      }
+      $html .= "<div class='pu-simple-annotation-list-meta'>{$user->user_login} on {$a->date}</div>";
+      $html .= "<div class='pu-simple-annotation-list-body'>{$a->annotation}</div>";
+      $html .= "</div>";
     }
   }
-  } // end if isset image path
-  ?>
+  return $html;
+}
 
-<?php
+
+
+/******************************************
+* add list below the image
+******************************************/
+
+function pusia_add_list() {
+  global $post;
+  $html = "";
+  if($annotations = get_post_meta($post->ID,'img_annotation')) {
+    $html .= "<div class='pu-simple-annotation-list' style='margin:10px 0 30px 0;'>";
+    foreach($annotations as $anno) {
+      $a = json_decode($anno);
+
+      $user = get_userdata($a->userid);
+      $html .= "<div class='pu-simple-annotation-list-item' id='list{$a->id}' style='display:none;'>";
+      $html .= "<div class='pu-simple-annotation-list-meta'>{$user->user_login} on {$a->date}</div>";
+      $html .= "<div class='pu-simple-annotation-list-body'>{$a->annotation}</div>";
+      $html .= "</div>";
+    }
+    $html .= "</div>";
+  }
+  return $html;
+}
+
+
+/******************************************
+* add annotation form
+******************************************/
+
+function pusia_toolbar() {
+  global $post;
+  $html = "";
+  $html .= "<div id='simple_image_annotation-toolbar' style='top:0px;left:0px;display:none;'>";
+  $html .= " <div id='simple_image_annotation-toolbar-nav'><span id='simple_image_annotation-toolbar-close' style='font-size:1.3em;margin-right:0px;margin-bottom: 10px;' class='dashicons dashicons-dismiss'></span></div>";
+
+  // the form
+  $html .= "<form name='pusia-anno' id='pusia-anno' method='POST' action='' style='margin-bottom:0px;'>";
+  $html .= "  <input id='pusia-postid' type='hidden' name='postid' value='{$post->ID}'/>";
+  $html .= "  <input id='pusia-left' type='hidden' name='left'/>";
+  $html .= "  <input id='pusia-top' type='hidden' name='top'/>";
+  $html .= "  <textarea name='annotation' id='annotation' style='height:120px;width:100%' required></textarea>";
+  $html .= "  <input type='submit' style='margin-top:5px;width:100%;height:40px;background:#333;color:white;' value='Add'/>";
+  $html .= "</form>";
+  // end the form
+
+  $html .= "</div>";
+
+  return $html;
+}
+
+
+
+
+/******************************************
+* process form input
+******************************************/
+
+add_action( 'init', 'sprachpraxis_process_post' );
+
+function sprachpraxis_process_post() {
+
+     if( isset( $_POST['annotation'] ) ) {
+
+	  $postid = $_POST['postid'];
+
+	  $anno = new StdClass();
+
+
+	  $anno->id = str_pad(mt_rand(1,99999999),8,'0',STR_PAD_LEFT);
+	  // add current user id
+	  $anno->userid = get_current_user_id();
+	  // add current date to post data
+	  $anno->date = date("Y/m/d");
+
+	  $anno->top = $_POST['top'];
+	  $anno->left = $_POST['left'];
+
+	  $annotation = $_POST['annotation'];
+	  $annotation = str_replace("\r\n","**BR**",$annotation);
+	  $annotation = str_replace("\r","**BR**",$annotation);
+	  $annotation = str_replace("\n","**BR**",$annotation);
+	  $annotation = str_replace("'","**APOS**",$annotation);
+	  $annotation = str_replace('"',"**QUOT**",$annotation);
+	  //$annotation = nl2br($_POST['annotation']);
+
+	  // $annotation  = str_replace("<br />","**BR**",$annotation);
+	  //$annotation  = str_replace("\r","",$annotation);
+	  // $annotation  = str_replace("\n","",$annotation);
+	  //$anno->annotation = htmlspecialchars(addslashes($annotation), ENT_QUOTES);
+	  $anno->annotation = stripslashes($annotation);
+	  $data = json_encode($anno, JSON_UNESCAPED_UNICODE);
+
+	  add_post_meta( $postid, 'img_annotation', $data );
+
+     } // end if
+
+
+     if( isset( $_POST['action'] ) && $_POST['action'] == 'remove' ) {
+	$postid = $_POST['pid'];
+	$rel = str_replace('anno','',$_POST['rel']);
+
+	$annos = get_post_meta($postid, 'img_annotation');
+	foreach($annos as $anno) {
+	  $c = json_decode($anno);
+	  if($c->id == $rel) { delete_post_meta($postid, 'img_annotation', $anno); }
+	  $arr = array('success'=>'1');
+	  echo json_encode($arr);
+	  die();
+	  wp_die();
+	}
+
+     } // end if
+
 
 }
 
-add_action( 'add_meta_boxes', 'ImgAnno_add_meta_box' );
 
 
 
 
-
-
-
-
-
-/***********************************
- * Shortcode
-***********************************/
-
-add_shortcode( '', 'img_anno_shortcode_content' );
-
-function img_anno_shortcode_content($atts = [], $content = null)
-{
-    global $htmlcontent;
-    $htmlcontent .= $content;
-
-}
-
-echo "{$htmlcontent}";
-
-?>
